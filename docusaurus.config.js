6f9210a890a190fb678fcb1df116ca26d27a78f3
Trinity Stake <114076168+trinitystake@users.noreply.github.com>
2026-07-27 00:11:19 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,8 @@ const config = {
   url: 'https://trinitystake.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website/',
-  //baseUrl: '/',
+  //baseUrl: '/website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
